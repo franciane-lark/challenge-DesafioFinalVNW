3beb34ca03f081601e3e091c0a34d446a4d009ff
Franciane Lark <franciane.lark019@gmail.com>
2022-10-19 16:02:27 -0400

    update routes

---
+++ desafio-final/src/Routes/Router.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import About from "../Components/About/About";
+import Works from "../Components/Works/Works";
+import Home from "../Components/Home/Home";
+import Navbar from "./NavBar";
+
+export default function Router (){
+    return(
+    <BrowserRouter>
+    <Navbar>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/About" element={<About/>} />
+            <Route path="/Works" element={<Works/>} />
+        </Routes>
+    </Navbar>
+    </BrowserRouter>
+    );
+}
